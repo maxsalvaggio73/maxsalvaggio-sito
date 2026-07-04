a5a47b31bb4baffdfa395916fd5d36e12642dca1
Max Salvaggio <maxsalvaggio@gmail.com>
2026-07-04 20:20:51 +0200

    Carica configurazione Supabase per abilitare login online

---
+++ config.js
@@ -0,0 +1,5 @@
+// Configurazione locale connessione Supabase (Ignorata in Git)
+const config = {
+  SUPABASE_URL: "https://dtqcwngdaokwiotjxykg.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_mY0WKttHSjO12PwkgI8Xlg_1f_8zMDN"
+};
