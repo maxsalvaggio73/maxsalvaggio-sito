bbdf0fb0f7e69a05fe8704322bf2574f1b9033f5
Max Salvaggio <maxsalvaggio@gmail.com>
2026-07-04 23:39:28 +0200

    Update navigation: replace Film Work with Bio & Contact and add Area riservata

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 // Configurazione locale connessione Supabase (Ignorata in Git)
 const config = {
   SUPABASE_URL: "https://dtqcwngdaokwiotjxykg.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_mY0WKttHSjO12PwkgI8Xlg_1f_8zMDN"
+  SUPABASE_ANON_KEY: "sb_publishable_mY0WKttHSjO12PwkgI8Xlg_1f_8zMDN",
+  // URL di reindirizzamento dopo il reset della password (usata in login.html)
+  PASSWORD_RECOVERY_REDIRECT_URL: "https://www.maxsalvaggio.com/login.html"
 };
