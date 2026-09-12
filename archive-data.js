73f8637c5077fe5d81a388d78f1c75ae8422063e
Max Salvaggio <maxsalvaggio@gmail.com>
2026-09-12 04:32:43 +0200

    chore(portfolio): auto-update images and archive data

--- archive-data.js
+++ archive-data.js
@@ -1,5 +1,5 @@
 // Database delle immagini generato automaticamente dallo script scan.py
-// Data di generazione: 2026-09-12T02:27:55.472715+00:00
+// Data di generazione: 2026-09-12T02:32:43.485282+00:00
 
 const portfolioData = {
   "overview": [
